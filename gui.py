1ab0ea3018602d29d1b4f4fd66b5031964ea123f
NishthaCh <67361090+NishthaCh@users.noreply.github.com>
2021-01-16 23:38:03 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -84,10 +84,6 @@ def show_vid2():
 if __name__ == '__main__':
     root=tk.Tk()   
     
-    img = ImageTk.PhotoImage(Image.open("logo.png"))
-    heading = Label(root,image=img,bg='black')
-    
-    heading.pack() 
     heading2=Label(root,text="Photo to Emoji",pady=20, font=('arial',45,'bold'),bg='black',fg='#CDCDCD')                                 
     
     heading2.pack()
